5f1ab15215ba0120bc7f0435d5375fba158888e0
josna7jo <ltjosnajohnson2021@gmail.com>
2026-09-04 18:55:31 +0530

    feat:add arithmetic operators program

---
+++ src/arithmetic_operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ int main(){
+
+//arithmetic operators =,+,-,*,/,%
+int x = 10;
+int y = 2;
+int z =0;
+//z = x+y;
+//z = x-y
+//z = x*y;
+//z = x%y;  use float 
+
+z = x-y;
+ printf("%d\n",z);
+  x++;
+  printf("%d\n",x);
+
+ return 0;
+ }
