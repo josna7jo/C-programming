afaee7059bec123f032f5b1a9f4a933fb36e720c
josna7jo <ltjosnajohnson2021@gmail.com>
2026-09-07 21:18:59 +0530

    feat:Add the math_function file

---
+++ src/math_functions.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    int x = 2;
+    // x=sqrt(x);
+//x= pow(x,2);
+//x=abs(x);
+//x=ceil(x);
+//x=floor(x);
+//x=round(x);
+//x=log(x);
+//x=sin(x);
+//x=cos(x);
+
+    printf("%d", x);
+
+
+     return 0;
+}
