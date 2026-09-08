6dd02a811259432f27a55b9353cd4e85880bfcb0
josna7jo <ltjosnajohnson2021@gmail.com>
2026-09-08 22:51:31 +0530

    feat: Add the if statement file

---
+++ src/if_statement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int age = 0;
+printf("Enter your age: ");
+scanf("%d", &age);
+    if(age >= 65){
+        printf("you are a senior");
+    }
+   else if(age >= 18){
+        printf("You are an adult");
+    } 
+    else if(age ==0){
+        printf("You are a newborn.");
+        }
+    else{
+        printf("you are a child");
+    }
+    
+    return 0;
+}
